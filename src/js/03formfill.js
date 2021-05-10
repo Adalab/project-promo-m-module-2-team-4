effc5dc6efe0b4da130fab7dad18b3a6f94d5747
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-05-10 20:07:23 +0200

    add fill js

---
+++ src/js/03formfill.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const data={
+    fullName:document.querySelector("#name"),
+    position:document.querySelector('#position'),
+    image:document.querySelector('.label'),
+    email:document.querySelector("#email"),
+    phone:document.querySelector("#telephone"),
+    linkedin:document.querySelector("#linkedIn"),
+    github:document.querySelector("#github")
+}
+function changeTitle(){
+    const titleValue = name.value;
+    card.title.innerHTML= titleValue;
+
+}
+data.fullName.addEventListener('keyup', changeTitle);
