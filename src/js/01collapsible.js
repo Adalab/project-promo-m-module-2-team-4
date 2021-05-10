38d041e4f4471bd1c87bc1cdc14ee8ae08385e2a
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-05-10 13:14:03 +0200

    add collapsibles

--- src/js/01collapsible.js
+++ src/js/01collapsible.js
@@ -1,16 +1,54 @@
-"use strict"
+'use strict'
 
+//collapsible
 const cdesign=document.querySelector(".js-cdesign");
 const cfill=document.querySelector(".js-cfill");
 const cshare=document.querySelector(".js-cshare");
 
 const contentDesign=document.querySelector(".js-design");
-
 const contentFill=document.querySelector(".js-fill");
-
 const contentShare=document.querySelector(".js-share");
 
 contentFill.classList.add('hidden');
 contentShare.classList.add('hidden');
 
-console.log (contentFill);
+function hiddenDesign(){
+contentDesign.classList.add('hidden');
+// contentDesign.classList.remove('hidden');
+}
+function hiddenFill (){
+    contentFill.classList.remove('hidden');
+}
+function hiddenShare (){
+    contentShare.classList.remove('hidden');
+}
+
+function showDesign (){
+    hiddenDesign();
+    if (contentDesign.classList.contains('js-design')){
+        contentFill.classList.add('hidden');
+        contentShare.classList.add('hidden');
+        contentDesign.classList.remove('hidden');
+    }
+}
+
+function showFill (){
+    hiddenFill()
+    if (contentFill.classList.contains('js-fill')){
+        contentDesign.classList.add('hidden');
+        contentShare.classList.add('hidden');
+    }
+}
+
+function showShare (){
+    hiddenShare()
+    if (contentShare.classList.contains('js-share')){
+        contentDesign.classList.add('hidden');
+        contentFill.classList.add('hidden');
+    }
+}
+
+cdesign.addEventListener('click', showDesign);
+cfill.addEventListener('click', showFill);
+cshare.addEventListener('click', showShare);
+
