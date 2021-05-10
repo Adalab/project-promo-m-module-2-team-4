d8c74b75da51f9c8caa49bbf94fd5c1226c90195
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-05-10 12:43:17 +0200

    add js

---
+++ src/js/01collapsible.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const cdesign=document.querySelector(".js-cdesign");
+const cfill=document.querySelector(".js-cfill");
+const cshare=document.querySelector(".js-cshare");
+
+const contentDesign=document.querySelector(".js-design");
+
+const contentFill=document.querySelector(".js-fill");
+
+const contentShare=document.querySelector(".js-share");
+
+contentFill.classList.add('hidden');
+contentShare.classList.add('hidden');
+
+console.log (contentFill);
