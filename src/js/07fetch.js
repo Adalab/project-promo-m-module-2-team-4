ec9fd6c5f40bbda24d72d3f132982ae3a62d5ba5
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-05-19 17:25:23 +0200

    add fecth

--- src/js/07fetch.js
+++ src/js/07fetch.js
@@ -1,32 +1,35 @@
 const createCard = document.querySelector(".js-createcard");
 const responseElement = document.querySelector(".js-response");
+const btnTwitter = document.querySelector(".js-twitter");
 
 function handlerClickCreate(event) {
   event.preventDefault();
   if (data.fullName === "") {
     responseElement.innerHTML = "Oops! Tienes que rellenar tu nombre!";
-    responseElement.classList.remove(hidden);
+    responseElement.classList.remove("hidden");
   } else if (data.position === "") {
     responseElement.innerHTML =
       "Oops! Tienes que rellenar tu puesto de trabajo!";
-    responseElement.classList.remove(hidden);
-  } else if (data.image === "") {
-    responseElement.innerHTML = "Oops! Tienes que subir tu foto!";
-    responseElement.classList.remove(hidden);
-  } else if (data.email === "") {
+    responseElement.classList.remove("hidden");
+  }
+  // else if (data.image === "") {
+  //   responseElement.innerHTML = "Oops! Tienes que subir tu foto!";
+  //   responseElement.classList.remove("hidden");
+  // }
+  else if (data.email === "") {
     responseElement.innerHTML = "Oops! Tienes que rellenar tu email!";
-    responseElement.classList.remove(hidden);
+    responseElement.classList.remove("hidden");
   } else if (data.phone === "") {
     responseElement.innerHTML = "Oops! Tienes que rellenar tu teléfono!";
-    responseElement.classList.remove(hidden);
+    responseElement.classList.remove("hidden");
   } else if (data.linkedin === "") {
     responseElement.innerHTML =
       "Oops! Tienes que rellenar tu usuario de LinkedIn!";
-    responseElement.classList.remove(hidden);
+    responseElement.classList.remove("hidden");
   } else if (data.github === "") {
     responseElement.innerHTML =
       "Oops! Tienes que rellenar tu usuario de GitHub!";
-    responseElement.classList.remove(hidden);
+    responseElement.classList.remove("hidden");
   } else {
     fetch("https://awesome-profile-cards.herokuapp.com/card", {
       method: "POST",
@@ -37,10 +40,23 @@ function handlerClickCreate(event) {
       .then((data) => {
         if (data.success === false) {
           responseElement.innerHTML = "Te falta algun dato ;)";
-          responseElement.classList.remove(hidden);
+          responseElement.classList.remove("hidden");
         } else {
           /*responseElement.innerHTML = "La tarjeta ha sido creada!" + */
+          const newElement = document.createElement("p");
+          const newContent = document.createTextNode(
+            "La tarjeta ha sido creada"
+          );
+          newElement.appendChild(newContent);
+          responseElement.appendChild(newElement);
+
+          const newLink = document.createElement("a");
+          newLink.href = `${data.cardURL}`;
+          responseElement.appendChild(newLink);
+          btnTwitter.classList.remove("hidden");
         }
       });
   }
 }
+
+createCard.addEventListener("click", handlerClickCreate);
