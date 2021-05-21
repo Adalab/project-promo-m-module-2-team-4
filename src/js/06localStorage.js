ca91612d5da8eb3a5df2f2c86ffc5f63022ed762
NurAhissami <nurahi10@gmail.com>
2021-05-21 12:50:11 +0200

    Add validatin

--- src/js/06localStorage.js
+++ src/js/06localStorage.js
@@ -1,27 +1,20 @@
 function getLocalStorage() {
-  //   if (data !== null) {
-  //     nameInput.value = savedData.name;
-  //     jobInput.value = savedData.job;
-  //     emailInput.value = savedData.email;
-  //     phoneInput.value = savedData.phone;
-  //     linkedinInput.value = savedData.linkedin;
-  //     githubInput.value = savedData.github;
-  //   }
-  null != getlocalData &&
-    ((nameInput.value = getlocalData.name),
-    (jobInput.value = getlocalData.job),
-    (emailInput.value = getlocalData.email),
-    (telephoneInput.value = getlocalData.phone),
-    (linkedInInput.value = getlocalData.linkedin),
-    (githubInput.value = getlocalData.github),
-    (paletteSelector.value = getlocalData.palette),
-    (previewImg.value = getlocalData.photo),
-    (data = getlocalData)),
-    localStorageColor();
-}
-getLocalStorage();
+  if (data !== null) {
+    nameInput.value = savedData.name;
+    jobInput.value = savedData.job;
+    emailInput.value = savedData.email;
+    phoneInput.value = savedData.phone;
+    linkedinInput.value = savedData.linkedin;
+    githubInput.value = savedData.github;
+  }
 
-if (getlocalData) {
-  data = getlocalData;
+  localStorageColor();
+}
+if (localStorage.getItem("data") !== null) {
+  getLocalStorage();
+}
+
+if (savedData) {
+  data = savedData;
   inputChange();
 }
