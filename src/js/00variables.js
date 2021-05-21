23fdf6c1560e7758ced1f5f11994db37efd27fb3
NurAhissami <nurahi10@gmail.com>
2021-05-21 11:57:04 +0200

    Add docs

--- src/js/00variables.js
+++ src/js/00variables.js
@@ -37,14 +37,14 @@ let card = {
 // const paletteData = document.querySelector(".design");
 
 function getLocalStorage() {
-  if (data != null) {
-    nameInput.value = savedData.name;
-    jobInput.value = savedData.job;
-    emailInput.value = savedData.email;
-    phoneInput.value = savedData.phone;
-    linkedinInput.value = savedData.linkedin;
-    githubInput.value = savedData.github;
-  }
+  // if (data != null) {
+  nameInput.value = savedData.name;
+  jobInput.value = savedData.job;
+  emailInput.value = savedData.email;
+  phoneInput.value = savedData.phone;
+  linkedinInput.value = savedData.linkedin;
+  githubInput.value = savedData.github;
+  // }
 
   localStorageColor();
 }
